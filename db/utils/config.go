2f4dcda644dabc68b8a1bc6c395a1cc944eb6eaa
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-04-23 11:05:41 +0200

    added viper implementation for env vars

---
+++ db/utils/config.go
@@ -0,0 +1,27 @@
+package utils
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig returns the environment variables from app.env
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+	if err = viper.Unmarshal(&config); err != nil {
+		return
+	}
+
+	return
+}
