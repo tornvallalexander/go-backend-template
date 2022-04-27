82694a617cc8b8fd3d51e222f26cf3127eebb726
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-04-27 19:50:28 +0200

    added base implementation for token maker

---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+// Maker is interface for managing tokens
+type Maker interface {
+	// CreateToken creates a token with specified username and duration time
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken checks if the token is valid or not
+	VerifyToken(token string) (*Payload, error)
+}
---
+++ token/payload.go
@@ -0,0 +1,31 @@
+package token
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+// NewPayload creates a new token payload with specified username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
