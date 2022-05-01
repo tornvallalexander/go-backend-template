62043c2d7f106dffed2e73c5aed3785570e7ca7b
Alexander Törnvall <52507655+tornvallalexander@users.noreply.github.com>
2022-05-01 11:04:05 +0200

    added middleware authentication + test for complete coverage (#10)
    * added base middleware implementation
    
    * added base middleware implementation with OK test
    
    * added more test cases for complete testing of middleware
    
    Co-authored-by: Alexander Törnvall <alexander.tornvall@icloud.com>

---
+++ api/middleware.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/tornvallalexander/go-backend-template/token"
+	"net/http"
+	"strings"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayloadKey = "authorization_payload"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authorizationHeader := ctx.GetHeader(authorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			err := errors.New("unsupported authorization type")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+		}
+
+		authorizationToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(authorizationToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+		}
+
+		ctx.Set(authorizationPayloadKey, payload)
+		ctx.Next()
+	}
+}
